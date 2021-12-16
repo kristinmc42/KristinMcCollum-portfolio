0909f7fa8de789b2adba38010127a1724a6d08c5
kristinmc42 <mccollum.k.j@gmail.com>
2021-12-16 13:47:37 -0500

    added animation to intro image and added accessible tabbing to hamburger menu

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const app = {};
 
 
 
-// scroll to top feature
+// function will listen for scroll for both showing the scroll to top button, for animating the intro image, as well as hidinh the hamburger nav
 app.listenForScroll = () => {
-  // get button
+  // get scroll to top button
   const scrollButton = document.getElementById("scrollButton");
 
   // get root of document
   const rootElement = document.documentElement;
+
+  // get animation container
+  const animationContainerDiv = document.querySelector(".animationContainer");
+
+  // get hamburger nav and hamburger button
+  const navEl = document.getElementById("hamburgerNav");
+  const buttonEl = document.getElementById("hamburgerButton"); 
   
-  // add event listener for button click
+  // add event listener for to top button click
   scrollButton.addEventListener("click", () => {
     // function for scrolling to top
     rootElement.scrollTo({
@@ -23,13 +30,33 @@ app.listenForScroll = () => {
   document.addEventListener("scroll", () => {
     const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
   
+    // conditions for animating image
+    if ((rootElement.scrollTop / scrollTotal) > 0.04){
+      animationContainerDiv.classList.add("scrollActivate");
+    }else {
+      animationContainerDiv.classList.remove("scrollActivate");
+    }
+
+    // conditions for hiding hamburger menu
+    if ((rootElement.scrollTop / scrollTotal) > 0.1){
+      console.log("nav el is blurred")
+      navEl.style.display = "none";
+      console.log(buttonEl.children[0].className)
+      if (buttonEl.children[0].className === "fas fa-times"){
+        buttonEl.children[0].classList.toggle("fa-times");
+        buttonEl.children[0].classList.toggle("fa-bars");
+      }
+    }
+
+    // conditions for scroll to top button
     if ((rootElement.scrollTop / scrollTotal) > 0.15){
-      // show button
+      // show scroll to top button
       scrollButton.classList.add("showButton");
     }else {
-      // hide button
+      // hide scroll to top button
       scrollButton.classList.remove("showButton");
     }
+
   })
 }
 
@@ -55,20 +82,21 @@ app.showHamburgerMenu = () => {
   // get hamburger nav and button
   const navEl = document.getElementById("hamburgerNav");
   const buttonEl = document.getElementById("hamburgerButton");
+  // const hamburgerUL = document.getElementById("hamburgerUL");
   
   //add event listener
-  buttonEl.addEventListener('click', function(event){
+  buttonEl.addEventListener("click", function(event){
    // check that the icon was clicked
     if (event.target.tagName === "I"){
-      // if the icon is the bars(hamburger), show the nav
-      // if the icon is the times, hide the nav
-      if (event.target.className === 'fas fa-bars'){
-        navEl.style.display = 'block';
+      if (event.target.className === "fas fa-bars"){
+        // if the icon is the bars(hamburger), show the nav
+        navEl.style.display = "block";
       }else {
-        navEl.style.display = 'none';
+        // if the icon is the times, hide the nav
+        navEl.style.display = "none";
       }
-      event.target.classList.toggle('fa-bars');
-      event.target.classList.toggle('fa-times');
+      event.target.classList.toggle("fa-bars");
+      event.target.classList.toggle("fa-times");
     }
     
     // if user exxpands window while hamburger menu still open
@@ -89,12 +117,49 @@ app.showHamburgerMenu = () => {
   })
 }
 
+
+// event listener for keyboard press of hamburger button
+app.listenForTabActivateHamburger = () => {
+  const buttonEl = document.getElementById("hamburgerButton");
+
+  const navEl = document.getElementById("hamburgerNav");
+  
+  buttonEl.addEventListener("focus", (event) => {
+  
+      console.log("button focussed", event)
+
+      document.addEventListener("keyup", (event) =>{
+        console.log("key pressed", event)
+     
+        if (event.keyCode === 13 || event.keyCode === 32) {
+          console.log('space bar or enter pressed')
+          
+          // check that the hamburger icon was clicked
+          if (event.target.className === "hamburger"){
+            console.log(event.target.children[0].className)
+            if (event.target.children[0].className === "fas fa-bars"){
+              // if the icon is the bars(hamburger), show the nav
+              navEl.style.display = "block";
+              console.log("nav is showing")
+            }else {
+              // if the icon is the times, hide the nav
+              navEl.style.display = "none";
+              console.log("nav is hidden")
+            }
+            event.target.children[0].classList.toggle("fa-bars");
+            event.target.children[0].classList.toggle("fa-times");
+          }
+        }
+      })
+  })
+}
+
 // listen for toggle from light to dark mode
 app.listenForDarkModeToggle = () => {
   const toggle = document.getElementById("checkbox");
-  console.log(toggle)
+  // console.log(toggle)
   toggle.addEventListener("change", () => {
-    console.log("toggled")
+    // console.log("toggled")
     document.body.classList.toggle("darkMode");
   })
 }
@@ -106,6 +171,7 @@ app.init = () => {
   app.formSubmit();
   app.showHamburgerMenu();
   app.listenForDarkModeToggle();
+  app.listenForTabActivateHamburger();
 }
 
 app.init();
